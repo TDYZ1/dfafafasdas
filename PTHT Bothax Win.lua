10eae1f7b1c4d26a16f8a4805a6202f36f3f9fe3
TDYZ1 <96387616+TDYZ1@users.noreply.github.com>
2023-06-09 12:56:23 +0700

    Update PTHT Bothax Win.lua

--- PTHT Bothax Win.lua
+++ PTHT Bothax Win.lua
@@ -3,9 +3,11 @@
 plantId = 5640
 harvestId = 13593
 startY = 193
+endX = 199
 delayPlant = 150
 delayHarvest = 150
 command = "/config"
+farPlant = 8
 
 local stopPlant = false
 local stopHarvest = false
@@ -45,11 +47,13 @@ add_label|small| |left
 add_text_input|plantId|Plant ID|]]..plantId..[[|5|
 add_text_input|harvestId|Harvest ID|]]..harvestId..[[|5|
 add_text_input|startY|Start Y|]]..startY..[[|5|
+add_text_input|endX|End X|]]..endX..[[|5|
 add_text_input|delayPlant|Delay Plant|]]..delayPlant..[[|5|
 add_text_input|delayHarvest|Delay Harvest|]]..delayHarvest..[[|5|
+add_text_input|farPlant|Far Plant|]]..farPlant..[[|5|
 add_label|small| |left
-add_checkbox|planting|Plant|0
-add_checkbox|harvesting|Harvest|0
+add_checkbox|planting|Plants|0
+add_checkbox|harvesting|Harvests|0
 add_small_font_button|modfly|Turn Off ModFly|noflags|0|0|
 end_dialog|ptht|Cancel|Ok
 		]]
@@ -66,9 +70,11 @@ function IsReady(tile)
 function insert()
 	plantId = tonumber(packet:match("plantId|(.*)harvestId"))
 	harvestId = tonumber( packet:match("harvestId|(.*)startY"))
-	startY = tonumber(packet:match("startY|(.*)delayPlant"))
+	startY = tonumber(packet:match("startY|(.*)endX"))
+	endX = tonumber(packet:match("endX|(.*)delayPlant"))
 	delayPlant = tonumber(packet:match("delayPlant|(.*)delayHarvest"))
-	delayHarvest = tonumber(packet:match("delayHarvest|(.*)planting"))
+	delayHarvest = tonumber(packet:match("delayHarvest|(.*)farPlant"))
+	farPlant = tonumber(packet:match("farPlant|(.*)planting"))
 end
 --check checkbox value
 if types == 2 then
@@ -80,24 +86,32 @@ if types == 2 then
 	end
 	if packet:find("action|dialog_return\ndialog_name|ptht") then
 		insert()
-	end
-	if packet:find("planting|1") then
 		content = [[
 set_default_color|`o
-			
+
 add_label_with_icon|big|Edit PTHT Configuration|left|6840
 add_label|small| |left
 add_text_input|plantId|Plant ID|]]..plantId..[[|5|
 add_text_input|harvestId|Harvest ID|]]..harvestId..[[|5|
 add_text_input|startY|Start Y|]]..startY..[[|5|
+add_text_input|endX|End X|]]..endX..[[|5|
 add_text_input|delayPlant|Delay Plant|]]..delayPlant..[[|5|
 add_text_input|delayHarvest|Delay Harvest|]]..delayHarvest..[[|5|
+add_text_input|farPlant|Far Plant|]]..farPlant..[[|5|
 add_label|small| |left
-add_checkbox|planting|Plant|1
-add_checkbox|harvesting|Harvest|0
+add_checkbox|planting|Plants|0
+add_checkbox|harvesting|Harvests|0
 add_small_font_button|modfly|Turn Off ModFly|noflags|0|0|
 end_dialog|ptht|Cancel|Ok
-					]]
+		]]
+	end
+	if packet:find("planting|1\nharvesting|1") then
+		content = content:gsub("Plants|1","Plants|0")
+		content = content:gsub("Harvests|1","Harvests|0")
+		stopHarvest = true
+		stopPlant = true
+	elseif packet:find("planting|1") then
+		content = content:gsub("Plants|0","Plants|1")
 		stopHarvest = true
         stopPlant = false
 		if isPlantRunning then
@@ -108,45 +122,26 @@ end_dialog|ptht|Cancel|Ok
 				ChangeValue("[C] Modfly", true)
 				isPlantRunning = true
                 for y = startY, 0, -1 do
-                    for x = 0, 199,1 do
-                        if GetTile(x,y).fg == 0 then
-							FindPath(x,y,1000)
+                    for x = 0, endX,1 do
+                        if GetTile(x,y).fg == 0 and GetTile(x,y+1).fg ~= 0 and GetTile(x,y+1).fg ~= harvestId then
+							FindPath(x,y,delayPlant)
 							Sleep(delayPlant)
 							place(plantId)
 							Sleep(delayPlant)
-							FindPath(199,y,5000)
+							FindPath(x+farPlant,y,delayPlant)
 							Sleep(delayPlant)
 						end
-  							if stopPlant == true then
-								isPlantRunning = false
-								break
-							end
-                    end
 						if stopPlant == true then
 							isPlantRunning = false
 							break
 						end
+                    end
                 end
 				isPlantRunning = false
         end)
 		end
 	elseif packet:find("harvesting|1") then
-		content = [[
-set_default_color|`o
-
-add_label_with_icon|big|Edit PTHT Configuration|left|6840
-add_label|small| |left
-add_text_input|plantId|Plant ID|]]..plantId..[[|5|
-add_text_input|harvestId|Harvest ID|]]..harvestId..[[|5|
-add_text_input|startY|Start Y|]]..startY..[[|5|
-add_text_input|delayPlant|Delay Plant|]]..delayPlant..[[|5|
-add_text_input|delayHarvest|Delay Harvest|]]..delayHarvest..[[|5|
-add_label|small| |left
-add_checkbox|planting|Plant|0
-add_checkbox|harvesting|Harvest|0
-add_small_font_button|modfly|Turn Off ModFly|noflags|0|0|
-end_dialog|ptht|Cancel|Ok
-					]]
+		content = content:gsub("Harvests|0","Harvests|1")
         stopHarvest = false
 		stopPlant = true
 		if isHarvestRunning then
@@ -178,43 +173,8 @@ end_dialog|ptht|Cancel|Ok
 				isHarvestRunning = false	
         end)
 		end
-	elseif packet:find("planting|1\nharvesting|1") then
-		content = [[
-set_default_color|`o
-
-add_label_with_icon|big|Edit PTHT Configuration|left|6840
-add_label|small| |left
-add_text_input|plantId|Plant ID|]]..plantId..[[|5|
-add_text_input|harvestId|Harvest ID|]]..harvestId..[[|5|
-add_text_input|startY|Start Y|]]..startY..[[|5|
-add_text_input|delayPlant|Delay Plant|]]..delayPlant..[[|5|
-add_text_input|delayHarvest|Delay Harvest|]]..delayHarvest..[[|5|
-add_label|small| |left
-add_checkbox|planting|Plant|0
-add_checkbox|harvesting|Harvest|0
-add_small_font_button|modfly|Turn Off ModFly|noflags|0|0|
-end_dialog|ptht|Cancel|Ok
-					]]
-		stopHarvest = true
-		stopPlant = true
-		notif("Choose One Masbro")
 	elseif packet:find("planting|0\nharvesting|0") then
-		content = [[
-set_default_color|`o
-
-add_label_with_icon|big|Edit PTHT Configuration|left|6840
-add_label|small| |left
-add_text_input|plantId|Plant ID|]]..plantId..[[|5|
-add_text_input|harvestId|Harvest ID|]]..harvestId..[[|5|
-add_text_input|startY|Start Y|]]..startY..[[|5|
-add_text_input|delayPlant|Delay Plant|]]..delayPlant..[[|5|
-add_text_input|delayHarvest|Delay Harvest|]]..delayHarvest..[[|5|
-add_label|small| |left
-add_checkbox|planting|Plant|0
-add_checkbox|harvesting|Harvest|0
-add_small_font_button|modfly|Turn Off ModFly|noflags|0|0|
-end_dialog|ptht|Cancel|Ok
-					]]
+		content = content:gsub("Plants|1","Plants|0") or content:gsub("Harvests|1","Harvests|0")
 		stopHarvest = true
 		stopPlant = true
 		end
