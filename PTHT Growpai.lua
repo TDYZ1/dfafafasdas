d0c38d21cd4e392a626efe2ea8a3767d7cc22657
TDYZ1 <96387616+TDYZ1@users.noreply.github.com>
2023-06-18 21:02:59 +0700

    Update PTHT Growpai.lua

--- PTHT Growpai.lua
+++ PTHT Growpai.lua
@@ -25,20 +25,13 @@ function place(id)
 	SendPacketRaw(pkt)
 end
 
-function Hold()
-    local pkt = {}
-    pkt.type = 0
-    pkt.flags = 16779298
-    SendPacketRaw(pkt)
-end
-
 function notif(text)
 		var = {}
 		var[0] = "OnTextOverlay"
 		var[1] = text
 		var.netid = -1
 		SendVarlist(var)
-end 
+end
 
 content = [[
 set_default_color|`o
@@ -55,9 +48,10 @@ add_text_input|farPlant|Far Plant|]]..farPlant..[[|5|
 add_label|small| |left
 add_checkbox|planting|Plants|0
 add_checkbox|harvesting|Harvests|0
+add_checkbox|looping|Loop|0
 add_small_font_button|modfly|Turn Off ModFly|noflags|0|0|
 end_dialog|ptht|Cancel|Ok
-		]]
+]]
 
 notif("Script Terminated [PTHT]\n    `2Credit : `1Rtnt-#7940")
 
@@ -79,6 +73,7 @@ if types == 2 then
 		var[1] = content
 	    var.netid = -1
 	    SendVarlist(var)
+		return true
 	end
 	if packet:find("action|dialog_return\ndialog_name|ptht") then
 		insert()
@@ -97,16 +92,77 @@ add_text_input|farPlant|Far Plant|]]..farPlant..[[|5|
 add_label|small| |left
 add_checkbox|planting|Plants|0
 add_checkbox|harvesting|Harvests|0
+add_checkbox|looping|Loop|0
 add_small_font_button|modfly|Turn Off ModFly|noflags|0|0|
 end_dialog|ptht|Cancel|Ok
 		]]
 	end
-	if packet:find("planting|1\nharvesting|1") then
+	if packet:find("planting|1\nharvesting|1\nlooping|1") or packet:find("planting|1\nharvesting|1") then
 	content = content:gsub("Plants|1","Plants|0")
 	content = content:gsub("Harvests|1","Harvests|0")
 	stopHarvest = true
 	stopPlant = true
 	notif("Choose One Masbro")
+elseif packet:find("looping|1") then
+	content = content:gsub("Loop|0","Loop|1")
+	stopHarvest = false
+	stopPlant = false
+	RunThread(function ()
+		while true do
+			place(plantId)
+			Sleep(500)
+			SendPacket(2,"action|dialog_return\ndialog_name|cheats\ncheck_gems|1\ncheck_autoplace|1")
+			Sleep(500)
+			EditToggle("ModFly", true)
+			for y = startY, 0, -1 do
+				for x = 0, endX,1 do
+					if GetTile(x,y).fg == 0 and GetTile(x,y+1).fg ~= 0 and GetTile(x,y+1).fg ~= harvestId then
+						FindPath(x,y,delayPlant)
+						Sleep(delayPlant)
+						place(plantId)
+						Sleep(delayPlant)
+						FindPath(x+farPlant,y,delayPlant)
+						Sleep(delayPlant)
+					end
+					if stopPlant == true then
+						isPlantRunning = false
+						break
+					end
+				end
+				if stopPlant == true then
+					isPlantRunning = false
+					break
+				end
+			end
+			isPlantRunning = false
+			Sleep(1000)
+			SendPacket(2,"action|dialog_return\ndialog_name|ultraworldspray")
+			Sleep(2000)
+			SendPacket(2,"action|dialog_return\ndialog_name|cheats\ncheck_gems|1\ncheck_autoplace|0")
+			EditToggle("ModFly", true)
+			Sleep(1000)
+			for y = startY, 0, -1 do
+				for x = 0, endX,1 do
+					if GetTile(x,y).fg == harvestId and IsReady(GetTile(x,y)) == true then
+						FindPath(x,y,delayHarvest)
+						Hold()
+						Sleep(delayHarvest)
+						place(18)
+						Sleep(delayHarvest)
+					end
+					if stopHarvest == true then
+						isHarvestRunning = false
+						break
+					end
+				end
+				if stopHarvest == true then
+					isHarvestRunning = false
+					break
+				end
+			end
+			isHarvestRunning = false
+		end
+	end)
 	elseif packet:find("planting|1") then
 		content = content:gsub("Plants|0","Plants|1")
 		stopHarvest = true
@@ -124,7 +180,7 @@ end_dialog|ptht|Cancel|Ok
 							break
 						else
 							if GetTile(x,y).fg == 0 and GetTile(x,y + 1).fg ~= 0 then
-								FindPath(x,y,100)
+								FindPath(x,y,delayPlant)
 								Sleep(delayPlant)
 								place(plantId)
 								Sleep(delayPlant)
@@ -153,10 +209,8 @@ end_dialog|ptht|Cancel|Ok
 						if stopHarvest then
 							break
 						else
-							if GetTile(x,y).fg == harvestId and GetTile(x,y).ready then
-								FindPath(x,y,100)
-								Hold()
-								Sleep(200)
+							if GetTile(x,y).fg == harvestId then
+								FindPath(x,y,delayHarvest)
 								place(18)
 								Sleep(delayHarvest)
 							end
